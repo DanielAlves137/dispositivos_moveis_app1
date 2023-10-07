8e80c79fcf129c5c16662f83b3dc7fc3003f7ec2
Daniel Alves <danielalves137@gmail.com>
2023-10-07 00:47:12 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View , SafeAreaView, TouchableOpacity, Image} from 'react-native';
+import { styles } from './styles';
+import React, { useState } from 'react';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+
+  let img = 'https://avatars.githubusercontent.com/u/25805709?s=400&u=45ff669e27e1511275407bb77e08d36516b9b746&v=4';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <Image
+          source = {{ uri: img }}
+          style = {styles.imagem}
+        />
+      </SafeAreaView>
+      <View style={styles.dados}>
+        <Text style={styles.text}>Dados pessoais</Text>
+        <Text style={styles.text}>Daniel Alves</Text>
+      </View>  
+      <View style={styles.dados}>
+        <Text style={styles.text}>Formação</Text>
+        <Text style={styles.text}>Analise e Desenvolvimento</Text>
+      </View>
+      <View style={styles.dados}>
+        <Text style={styles.text}>Experiência</Text>
+        <Text style={styles.text}>tecnico por 3 anos</Text>
+      </View>
+      <View style={styles.dados}>
+        <Text style={styles.text}>Projetos</Text>
+        <Text style={styles.text}>9 TPs de Dispositivos Móveis</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    dados: {
+      alignItems: 'left',
+      justifyContent: 'center',
+      marginLeft: 10,
+      padding: 15,
+    },
+    text: {
+      color: 'green',
+      fontSize: 20,
+    },
+    imagem:{
+        width: 150,
+        height: 150,
+    },
+  
+});
+
+export { styles }
